98db7531daa29069d40239a688f58ae05bddbcfd
Gardar Hauksson <gardarh@payanalytics.com>
2022-10-16 21:53:29 +0200

    DEBUG, ALLOWED_HOSTS and SECRET_KEY can now be set with env vars.

--- django-app/htp/settings.py
+++ django-app/htp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-n3b4hxeht518+*t1trg*+97s(=7!=bb12pl^!^iy_qojj4o8o$"
+if os.getenv("DJANGO_SECRET_KEY") is not None:
+    SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+else:
+    SECRET_KEY = "django-insecure-n3b4hxeht518+*t1trg*+97s(=7!=bb12pl^!^iy_qojj4o8o$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "1") == "1"
 
-ALLOWED_HOSTS = []
+if os.getenv("DJANGO_ALLOWED_HOSTS") is not None:
+    ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(",")
+else:
+    ALLOWED_HOSTS = []
 
 
 # Application definition
